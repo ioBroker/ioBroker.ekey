9494bbbf4d4edc63ea7d227afe92f052d619877f
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:43:43 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 
 'use strict';
 
-const utils   = require('./lib/utils'); // Get common adapter utils
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const adapter = new utils.Adapter('ekey');
 const dgram   = require('dgram');
 let   devices = {};
